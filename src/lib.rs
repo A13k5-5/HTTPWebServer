5e3c460d644c1bc27c9bbdf09200d70318eb643a
alex <pisonalex@gmail.com>
2026-03-01 12:48:50 +0100

    Feat: worker threads take turns in taking in the requests

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub struct ThreadPool {
     sender: mpsc::Sender<Job>
 }
 
-struct Job;
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThredPool
@@ -36,6 +36,9 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -46,8 +49,14 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                println!("Worker {id} got a job; executing.");
+
+                job();
+            }
         });
 
         Worker { id, thread }
